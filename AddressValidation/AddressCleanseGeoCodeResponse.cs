e3a7d6e7f35709fbf7c0ea4b96b88601ac247d00
Richard West <rwest@peachtreedata.com>
2014-02-04 17:25:09 -0500

    Initial Commit

---
+++ AddressValidation/AddressCleanseGeoCodeResponse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressValidation
+{
+    public class AddressCleanseGeoCodeResponse
+    {
+        public string Status { get; set; }
+        public string StatusMessage { get; set; }
+        public string RawDetailCode { get; set; }
+        public string AddressID { get; set; }
+        public string PrimaryAddress { get; set; }
+        public string PrimaryNumber { get; set; }
+        public string PrimaryPrefix { get; set; }
+        public string PrimaryName { get; set; }
+        public string PrimaryType { get; set; }
+        public string PrimaryPostfix { get; set; }
+        public string PrimaryAddressRemainder { get; set; }
+        public string SecondaryAddress { get; set; }
+        public string SecondaryUnitDescription { get; set; }
+        public string SecondaryUnitNumber { get; set; }
+        public string City { get; set; }
+        public string City13 { get; set; }
+        public string State { get; set; }
+        public string Zip10 { get; set; }
+        public string Zip5 { get; set; }
+        public string Zip4 { get; set; }
+        public string AddressType { get; set; }
+        public string IsDefault { get; set; }
+        public string DeliveryPointBarcode { get; set; }
+        public string DeliveryPointBarcodeCheckDigit { get; set; }
+        public string LineOfTravel { get; set; }
+        public string LineOfTravelOrder { get; set; }
+        public string CarrierRoute { get; set; }
+        public string CountyCode { get; set; }
+        public string CountyName { get; set; }
+        public string DpvFootnote { get; set; }
+        public string IsDpvNoStat { get; set; }
+        public string DpvStatus { get; set; }
+        public string SuiteLinkResult { get; set; }
+        public string IsVacant { get; set; }
+        public string NonPostalSecondaryAddress { get; set; }
+        public string ExtraLine1 { get; set; }
+        public string ExtraLine2 { get; set; }
+        public string SecondaryAddressExtraneous { get; set; }
+        public string AssignmentLevel { get; set; }
+        public string CensusTractBLock { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string MetroStatAreaCode { get; set; }
+    }
+
+
+}
---
+++ AddressValidation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
+
+
+namespace AddressValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customerId = "";
+            string apikey = "";
+            string address = "address1=2905+premiere+parkway+ste+200&zipcode=30097";
+            string url = "https://rapid.peachtreedata.com/api/v1/AddressCleanseGeoCode";
+            string fullurl = url + "?CustomerID=" + customerId + "&APIKEY=" + apikey + "&" + address;
+
+            var client = new RestClient(url);
+            var request = new RestRequest();
+
+            request.AddParameter("CustomerID", "XXX999");
+            request.AddParameter("apikey", "e78f9d79-8710-4753-a099-88c95ea11d0f");
+            request.AddParameter("Address1", "2905 premiere parkway");
+            request.AddParameter("ZIPCode", "30097");
+            request.AddParameter("CompanyName", "peachtree  ");
+
+            var response = client.Execute <AddressCleanseGeoCodeResponse>(request);
+
+            Console.WriteLine("Primary Address:   {0}", response.Data.PrimaryAddress);
+            Console.WriteLine("Secondary Address: {0}", response.Data.SecondaryAddress);
+            Console.WriteLine("ZIP Code:          {0}", response.Data.Zip10);
+            Console.WriteLine("Latitude:          {0}", response.Data.Latitude);
+            Console.WriteLine("Longitude:         {0}", response.Data.Longitude);
+            Console.WriteLine("Geo Assignment:    {0}", response.Data.AssignmentLevel);
+            Console.WriteLine("Status:            {0}", response.Data.StatusMessage);
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
